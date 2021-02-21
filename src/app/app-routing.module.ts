9d1320a7f70edda0ca8b1e56e4e2e4d59d5ac987
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2021-02-21 16:13:55 -0500

    added routes for home and feature1 in the app-routing.modules.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent},
+  { path: 'feature1', component: Feature1Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomeComponent, Feature1Component]
