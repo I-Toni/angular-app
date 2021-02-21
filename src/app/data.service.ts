6acc836f7121158e4ec7689633351b285ef191cd
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2021-02-21 17:09:11 -0500

    set up the data.service.ts to include temp dataSet array and getters and setters.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -4,6 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DataService {
+  dataset: string[] = [];
 
   constructor() { }
+
+  
+    
+  ngOnInit() {
+  }
+
+  getDataSets() {
+    return this.dataset;
+  }
+  
+  addData(data: string) {
+    if (data === null ) {
+      throw new Error('Data can not be null');
+    };
+    if (data === '') {
+      throw new Error('Data must not be empty');
+    };
+    if (this.dataset.indexOf(data) >= 0 ) {
+      throw new Error('Data must not be duplicate');
+    };
+    this.dataset.push(data);
+  }
+  
+  removeContestants() {
+    this.dataset = [];
+  }
 }
